c5ce4d9874a4db55e2a0f745a04f56ee1c230ffb
Sabin546 <pasca_sabin@yahoo.com>
2024-06-17 21:55:46 +0300

    First Commit

--- src/Game.java
+++ src/Game.java
@@ -14,11 +14,20 @@ public class Game {
         Random rand = new Random();
         int hiddenNumber = rand.nextInt(101);
         int attempts = 0;
+        int lives = 8;
+
 
         while (true) {
             System.out.println("Guess a number between 1 and 100");
             int guessTheNumber = sc.nextInt();
             attempts++;
+            lives--;
+            System.out.println(lives + " lives left");
+
+
+            if (lives < 1){
+                System.out.println("Game Over!");
+            break;}
 
             if (guessTheNumber < hiddenNumber) {
                 System.out.println("more");
@@ -26,7 +35,7 @@ public class Game {
                 System.out.println("less");
             } else {
                 System.out.println("you guessed the number!");
-                System.out.println("You used " + attempts + " attempts.");
+                System.out.println("You used " + attempts + " attempts and you have " + lives + " lives left");
                 break;
 
 
